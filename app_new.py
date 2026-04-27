b210d9dc69108d3e115dca810a70883c0ce2c71f
jacques calame <jacquescalame@jacquess-MacBook-Pro.local>
2026-04-26 23:13:15 -0700

    initial commit

---
+++ app_new.py
@@ -0,0 +1,57 @@
+import pandas as pd
+from flask import Flask, render_template, request
+app = Flask(__name__)
+dishes = '/Users/jacquescalame/Desktop/Allergen_app/Allergens New.csv'
+df = pd.read_csv(dishes)
+df.columns = df.columns.str.strip().str.lower()
+for col in df.columns[1:]:
+    df[col] = (
+        df[col]
+        .astype(str)
+        .str.strip()
+        .str.lower()
+        .map({
+            "true": True,
+            "false": False,
+            "1": True,
+            "0": False,
+            "yes": True,
+            "no": False
+        })
+        .fillna(False)
+    )
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+
+    allergens = df.columns.tolist()[1:]
+
+    safe_results = []
+
+    if request.method == 'POST':
+        #print("FORM DATA:", request.form)
+        selected_allergens = [a.lower() for a in request.form.getlist('allergens')]
+        print("selected_allergens:", selected_allergens)
+
+        for _, row in df.iterrows():
+            is_safe =True
+
+            for a in selected_allergens:
+                if a in df.columns and row[a] is True:
+                    is_safe = False
+                    break
+
+            if is_safe:
+                safe_results.append({
+                    "dish": row.iloc[0],
+            })
+
+
+        return render_template('results.html', results= results)
+
+    return render_template('index.html', allergens= allergens)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
